cfe9eb563060ebc3c77cf0e9748ba577e58d0feb
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 11:51:40 +0100

    Probando formularios

--- app.py
+++ app.py
@@ -3,21 +3,19 @@ import joblib
 import pandas as pd
 from PIL import Image
 
-image = Image.open('sus.gif')
+image = Image.open('static/images/bateria.png')
 
-st.title("Streamlit Machine Learning App")
+st.title("Streamlit Mobile price ML")
 
 # Add a text input
-st.image(image, caption='among us')
-height = st.number_input("Enter your Height:", step=1.0)
-weight = st.number_input("Enter your Weight:", step=1.0)
-color_eyes = st.selectbox("Enter your color eyes:", ('Brown', 'Blue'))
+st.image()
+height = st.number_input("Enter the battery power in mAh:", step=1.0)
 submit = st.button("Submit")
 
 # Display the entered name
 if submit:
-    pickled_model = joblib.load(open('pet_model.pkl', 'rb'))
-    X = pd.DataFrame([[height, weight, color_eyes]], columns=["Height", "Weight", "Eye"])
-    X = X.replace(["Brown", "Blue"], [1, 0])
-    prediction = pickled_model.predict(X)[0]
-    st.text(prediction)
+    pickled_model = joblib.load(open('model/mobile_model.pkl', 'rb'))
+    # X = pd.DataFrame([[height, weight, color_eyes]], columns=["Height", "Weight", "Eye"])
+    # X = X.replace(["Brown", "Blue"], [1, 0])
+    # prediction = pickled_model.predict(X)[0]
+    # st.text(prediction)
