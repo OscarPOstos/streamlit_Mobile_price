6e17b31a6691e6309de389e4303ba6e2dc3d150d
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 12:08:14 +0100

    Probando formularios

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import joblib
-import pandas as pd
+# import joblib
+# import pandas as pd
 from PIL import Image
 
 image = Image.open('static/images/bateria.png')
@@ -14,7 +14,7 @@ submit = st.button("Submit")
 
 # Display the entered name
 if submit:
-    pickled_model = joblib.load(open('model/mobile_model.pkl', 'rb'))
+    # pickled_model = joblib.load(open('model/mobile_model.pkl', 'rb'))
     # X = pd.DataFrame([[height, weight, color_eyes]], columns=["Height", "Weight", "Eye"])
     # X = X.replace(["Brown", "Blue"], [1, 0])
     # prediction = pickled_model.predict(X)[0]
