e796a8a609ce6f02633798010891b242c9a1235f
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 16:40:31 +0100

    colocando inputs y modificando modelo

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ with columns2[1]:
     st.image(image, width= 200)
 with columns2[2]:
     frontal_camera = st.number_input("Enter the mega px of the frontal camera:", step=1.0)
+    height = st.number_input("Enter the height in px of the mobile", step=1.0)
+
+width = st.number_input("Enter the width in px of the mobile", step=1.0)
 submit = st.button("Submit")
 
 # Display the entered name
