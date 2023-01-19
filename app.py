2069f0d4d37e2609680a83cb52628a4d06cb018d
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 12:48:50 +0100

    Probando formularios

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ with columns[0]:
     image = Image.open('static/images/bateria.png')
     st.image(image, width=50)
 with columns[1]:
-    height = st.number_input("Enter the battery power in mAh:", step=100)
+    mAh = st.number_input("Enter the battery power in mAh:", step=100)
 with columns[2]:
     st.text("")
     st.text("")
     image = Image.open('static/images/procesador.png')
     st.image(image, width=50)
+with columns[3]:
+    clock_speed = st.number_input("Enter the clock speed:", step=1.0)
 submit = st.button("Submit")
 
 # Display the entered name
