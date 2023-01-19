c17c7eda1f1ddb61015d9a600a2303ed116d7224
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 18:15:38 +0100

    Establecer estilos

--- static/style/style.py
+++ static/style/style.py
@@ -4,6 +4,6 @@ import streamlit as st
 def set_style():
     st.markdown("""<style>
                 section.main.css-k1vhr4.egzxvld5 {
-                background-color: red
+                background-image: url("static/images/background.gif")
                 }
                 </style>""", unsafe_allow_html=True)
