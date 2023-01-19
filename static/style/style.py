1fe29eeca148dcb18bb022ab325cec1ac3823b35
OscarPOstos <theholderhaizaki@gmail.com>
2023-01-19 18:30:08 +0100

    Establecer estilos

--- static/style/style.py
+++ static/style/style.py
@@ -4,6 +4,12 @@ import streamlit as st
 def set_style():
     st.markdown("""<style>
                 section.main.css-k1vhr4.egzxvld5 {
-                background-image: url('static/images/background.gif')
+                    background-image: url(https://static.vecteezy.com/system/resources/previews/004/631/777/non_2x/black-triangular-modern-background-free-vector.jpg);
+                    background-size: cover;
+                }
+                
+                .block-container.css-91z34k.egzxvld4 {
+                    background-color: #ffffff4a;
+                    border-radius: 20px
                 }
                 </style>""", unsafe_allow_html=True)
